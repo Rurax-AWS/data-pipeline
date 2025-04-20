e971880230855da92a4a539a54375fef85fa17cb
Icaro Albar <icaro.albar@live.com>
2025-04-20 16:04:48 -0300

    :construction: feat: configuration file SQS

---
+++ main.py
@@ -0,0 +1,17 @@
+from utils.sqs_client import receive_messages, process_message, delete_message
+
+def event_pubsub_subscribe(event, context):
+    print("Verificando mensagens na fila SQS...")
+
+    while True:
+        messages = receive_messages(10)  
+
+        if not messages:
+            print("Nenhuma mensagem disponível na fila.")
+            break  
+
+        for message in messages:
+            process_message(message)
+            delete_message(message['ReceiptHandle'])
+
+    return "Todas as mensagens foram processadas com sucesso."
---
+++ utils/sqs_client.py
@@ -0,0 +1,27 @@
+import os
+import boto3
+from time import gmtime, strftime
+import json
+
+sqs = boto3.client('sqs', region_name='us-east-1')
+queue_url = os.environ['SQS_QUEUE_URL']
+
+def receive_messages(max_messages=10):
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=max_messages,
+        WaitTimeSeconds=5
+    )
+    return response.get('Messages', [])
+
+def process_message(message):
+    body = json.loads(message['Body'])
+    body['EVENT_INGEST'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    print("Mensagem processada:", json.dumps(body, indent=4))
+
+def delete_message(receipt_handle):
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    )
+    print(f"Mensagem com ReceiptHandle {receipt_handle} deletada da fila com sucesso.")
