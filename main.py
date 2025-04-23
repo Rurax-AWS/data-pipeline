f8e054e1fce885245a414c006c4a2f469b0b63e4
Icaro Albar <icaro.albar@live.com>
2025-04-23 20:42:41 -0300

    :bug: fix: ajustmente timestamp

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils.sqs_client import receive_messages, process_message, delete_message
 from utils.csv_convert import json_to_csv
 from utils.s3_client import upload_to_s3
 import os
+from time import gmtime, strftime
 
 def event_pubsub_subscribe(event, context):
     print("Verificando mensagens na fila SQS...")
@@ -17,12 +18,21 @@ def event_pubsub_subscribe(event, context):
             break
 
         for message in messages:
-            # Processa a mensagem
-            process_message(message)
+            # Gera o timestamp único
+
+            getgmtime = gmtime()
+            
+            timestamp_event_ingest = strftime("%Y-%m-%d %H:%M:%S", getgmtime)  
+            timestamp_csv = strftime("%Y%m%d_%H%M%S", getgmtime)
+
+            # Processa a mensagem e adiciona o timestamp no EVENT_INGEST
+            body = json.loads(message['Body'])
+            body['EVENT_INGEST'] = timestamp_event_ingest
+            print("Mensagem processada:", json.dumps(body, indent=4))
 
             # Converte a mensagem para CSV
-            data = [json.loads(message['Body'])]  # Envolvendo a mensagem em uma lista
-            file_name, csv_content = json_to_csv(data)
+            data = [body]  # Envolvendo a mensagem em uma lista
+            file_name, csv_content = json_to_csv(data, timestamp_csv)
 
             print(f"CSV gerado: {file_name}")
 
@@ -33,6 +43,11 @@ def event_pubsub_subscribe(event, context):
                 file_content=csv_content,  # Passa o conteúdo do CSV diretamente
                 extra_args={"ContentType": "text/csv"}  # Define o tipo de conteúdo
             )
+
+            if success:
+                print(f"Arquivo enviado com sucesso para o S3: s3://{bucket_name}/{file_name}")
+            else:
+                print(f"Falha ao enviar o arquivo para o S3: {file_name}")
 
             # Deleta a mensagem da fila
             delete_message(message['ReceiptHandle'])
--- utils/csv_convert.py
+++ utils/csv_convert.py
@@ -1,12 +1,18 @@
 import csv
-import os
-from time import gmtime, strftime
 from io import StringIO
 
-def json_to_csv(data):
+def json_to_csv(data, timestamp):
+    """
+    Converte JSON para CSV e retorna o nome do arquivo e o conteúdo CSV.
 
-    time_now = strftime("%Y%m%d_%H%M%S", gmtime())
-    file_name = f"{time_now}.csv"  
+    Args:
+        data (list of dict): Lista de dicionários a serem convertidos para CSV.
+        timestamp (str): Timestamp único para o nome do arquivo.
+
+    Returns:
+        tuple: (nome_do_arquivo, conteúdo_csv)
+    """
+    file_name = f"{timestamp}.csv"  # Nome do arquivo com o timestamp
 
     csv_buffer = StringIO()
     writer = csv.DictWriter(csv_buffer, fieldnames=data[0].keys())
