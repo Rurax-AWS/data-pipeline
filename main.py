f370f07008538e612062c5816be4cef169e71c32
Icaro Albar <icaro.albar@live.com>
2025-04-23 00:05:44 -0300

    :construction: feat: convert json for csv and add in bucket s3

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+import json 
 from utils.sqs_client import receive_messages, process_message, delete_message
+from utils.csv_convert import json_to_csv
+from utils.s3_client import upload_to_s3
 
 def event_pubsub_subscribe(event, context):
-    # print("Verificando mensagens na fila SQS...")
+    print("Verificando mensagens na fila SQS...")
+
+    # Defina o prefixo para o caminho no S3
+    prefix = "datasets/test"
 
     while True:
         messages = receive_messages(10)
         if not messages:
-            # print("Nenhuma mensagem disponível na fila.")
+            print("Nenhuma mensagem disponível na fila.")
             break
 
         for message in messages:
-            # print(f"Processando mensagem: {message['MessageId']}")
+            # Processa a mensagem
             process_message(message)
+
+            # Converte a mensagem para CSV e salva uma cópia local
+            data = [json.loads(message['Body'])]  # Envolvendo a mensagem em uma lista
+            file_name, csv_content = json_to_csv(data, prefix)
+
+            print(f"CSV gerado: {file_name}")
+
+            # Deleta a mensagem da fila
             delete_message(message['ReceiptHandle'])
 
     return "Todas as mensagens foram processadas com sucesso."
---
+++ utils/csv_convert.py
@@ -0,0 +1,16 @@
+import csv
+import os
+from time import gmtime, strftime
+from io import StringIO
+
+def json_to_csv(data, prefix):
+
+    time_now = strftime("%Y%m%d_%H%M%S", gmtime())
+    file_name = f"{prefix}/{time_now}.csv"  
+
+    csv_buffer = StringIO()
+    writer = csv.DictWriter(csv_buffer, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
+
+    return file_name, csv_buffer.getvalue()
