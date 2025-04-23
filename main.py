c538756343ec5428121dc9a5af6089e7101f486f
Icaro Albar <icaro.albar@live.com>
2025-04-23 20:16:23 -0300

    :construction: feat: save backup in bucket S3

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import json 
+import json
 from utils.sqs_client import receive_messages, process_message, delete_message
 from utils.csv_convert import json_to_csv
 from utils.s3_client import upload_to_s3
+import os
 
 def event_pubsub_subscribe(event, context):
     print("Verificando mensagens na fila SQS...")
 
-    # Defina o prefixo para o caminho no S3
-    prefix = "datasets/test"
+    # Configurações do S3
+    bucket_name = os.getenv("S3_BUCKET_BACKUP")
 
     while True:
         messages = receive_messages(10)
@@ -19,11 +20,19 @@ def event_pubsub_subscribe(event, context):
             # Processa a mensagem
             process_message(message)
 
-            # Converte a mensagem para CSV e salva uma cópia local
+            # Converte a mensagem para CSV
             data = [json.loads(message['Body'])]  # Envolvendo a mensagem em uma lista
-            file_name, csv_content = json_to_csv(data, prefix)
+            file_name, csv_content = json_to_csv(data)
 
             print(f"CSV gerado: {file_name}")
+
+            # Faz o upload do CSV para o S3
+            success = upload_to_s3(
+                bucket_name=bucket_name,
+                object_name=file_name,
+                file_content=csv_content,  # Passa o conteúdo do CSV diretamente
+                extra_args={"ContentType": "text/csv"}  # Define o tipo de conteúdo
+            )
 
             # Deleta a mensagem da fila
             delete_message(message['ReceiptHandle'])
--- utils/csv_convert.py
+++ utils/csv_convert.py
@@ -3,10 +3,10 @@ import os
 from time import gmtime, strftime
 from io import StringIO
 
-def json_to_csv(data, prefix):
+def json_to_csv(data):
 
     time_now = strftime("%Y%m%d_%H%M%S", gmtime())
-    file_name = f"{prefix}/{time_now}.csv"  
+    file_name = f"{time_now}.csv"  
 
     csv_buffer = StringIO()
     writer = csv.DictWriter(csv_buffer, fieldnames=data[0].keys())
